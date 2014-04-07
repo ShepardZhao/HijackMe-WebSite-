923418eb3d9ade4da1f68296f5913d37e6b68442
shepard.zhao <zhaoxun321@gmail.com>
2014-04-08 01:08:10 +1000

    Project Initialisation
    Project Initialisation

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<link rel="stylesheet" href="css/normalize.css">
+  	<link rel="stylesheet" href="css/foundation.min.css">
+  	<script src="js/jquery-1.11.0.min.js"></script>
+  	<script src="js/foundation.js"></script>
+  	<script src="js/foundation.reveal.js"></script>
+	<![endif]-->
+</head>
+<body>
+	
+</body>
+</html>
