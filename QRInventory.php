b4942de0f898aaaedaf725733db89e5f571e5059
shepard.zhao <zhaoxun321@gmail.com>
2014-05-05 14:59:12 +1000

    test website
    test website

---
+++ QRInventory.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <a class="close-reveal-modal">&#215;</a>
+      <!-- tags zone-->
+      <div class="large-12 columns" id="QRTagezone">
+
+
+      </div>
+
+      <!-- QR list -->
+      <div class="large-12 columns" id="QRlist">
+      </div>
+
+
+</div>
